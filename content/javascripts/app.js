bf1883ab5f8c1289bf11474e2be3a95345ac975b
Lang Sharpe <langer8191@gmail.com>
2016-08-17 13:51:05 +1000

    Remove API status from menu.
    The status endpoint no longer accepts jsonp, and doesn't have CORS
    headers. It is also bad.

--- content/javascripts/app.js
+++ content/javascripts/app.js
@@ -1,12 +1,4 @@
 $(function() {
-  var $container = $('#api-status span');
-
-  $.getJSON('http://everydayhero.com/api/v2/status.jsonp?callback=?', function(data) {
-    $container.addClass('available');
-    $container.removeClass('down');
-    $container.text('Available');
-  });
-
   $('#run').click(function() {
     load_api_content($('#path').val());
   });
