f50b39f18b70d03eb5d5f7173043e6887abc2bae
joel <jo@easyjo.com>
2016-08-24 09:07:29 +1000

    Update pages.md

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,22 +1,4 @@
 $(function() {
-  var $container = $('#api-status span');
-
-  $.getJSON('http://everydayhero.com/api/v2/status.jsonp?callback=?', function(data) {
-    $container.addClass('available');
-    $container.removeClass('down');
-    $container.text('Available');
-  });
-
-  $('#run').click(function() {
-    load_api_content($('#path').val());
-  });
-
-  var query = $.url().param('query');
-  if (query) {
-    $('#path').val(query);
-    load_api_content(query);
-  }
-
   function heightCheck() {
     var sideBarHeight = $('.side-bar').height();
 
